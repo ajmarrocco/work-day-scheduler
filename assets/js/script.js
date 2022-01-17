4268605ade57e7b15db4acf33b7e0bb6d412ad27
ajmarrocco <ajmarrocco@gmail.com>
2022-01-17 11:24:09 -0500

    save tasks in create card function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,24 @@ var hours = 18;
 //sets text of currentTimeEl to current day
 currentTimeEl.text(moment().format("dddd, MMMM Do"));
 
+// var saveTasks = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+// };
+
+// var loadTasks = function() {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+
+//     // if nothing in localStorage, create a new object to track all task status arrays
+//     if (!tasks) {
+//         tasks = {
+//             toDo: [],
+//             inProgress: [],
+//             inReview: [],
+//             done: []
+//         };
+//     }
+// };
+
 //colors the time blocks
 var colorBlocks = function(timeAndDay, index){
     // gets current time and sets as value
@@ -30,26 +48,34 @@ var colorBlocks = function(timeAndDay, index){
 //creates card container
 var createCard = function(text, index){
     var cardContainerEl = $("#time-blocks");
-    //sets var for label and adds text and class
-    var labelEL = $("<label>")
+    //sets var for label and adds text and class and div based on index
+    var divEl = $("<div>")
+        .addClass("card")
+        .attr('id', 'div' + index);
+    var labelEl = $("<label>")
         .text(text)
-        .addClass("col-1");
+        .addClass("col-1 d-inline-flex");
     //sets var for input and adds text and class
-    var inputEL = $("<input>")
+    var inputEl = $("<input>")
         .attr('id', 'hour' + index)
-        .addClass("col-10");
+        .addClass("col-10 d-inline-flex");
     //sets var for button and class
-    var buttonEL = $("<button>")
-        .addClass("col-1");
+    var buttonEl = $("<button>")
+        .addClass("col-1")
+        .attr('id', 'btn' + index);
     //adds icon to span
     var spanEl = $("<span>")
         .addClass("oi oi-lock-locked")
     //appends div and input, label, button to cardContainer
-    cardContainerEl.append("<div class='card'></div>");
-    cardContainerEl.append(labelEL,inputEL,buttonEL);
-    //appends span to button
-    buttonEL.append(spanEl);
+    cardContainerEl.append(divEl);
+    $(divEl).append(labelEl,inputEl,buttonEl);
 
+    //appends span to button
+    buttonEl.append(spanEl);
+
+    $("#div" + index).on("click", "button", function(){
+        localStorage.setItem(labelEl.text(), inputEl.val());
+    })
 }
 
 //adds AM or PM to time and changes zero to 12
